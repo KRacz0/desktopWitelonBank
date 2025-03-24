6e07b7bcb809238ce2e0d2de0d8d1e86d81a2fde
Krystian <theaxotv@gmail.com>
2025-03-24 18:01:43 +0100

    Build project

---
+++ src/main/java/com/kracz0/desktopwitelonbank/Views/ViewFactory.java
@@ -0,0 +1,4 @@
+package com.kracz0.desktopwitelonbank.Views;
+
+public class ViewFactory {
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -10,7 +10,11 @@ module com.kracz0.desktopwitelonbank {
     requires org.kordamp.bootstrapfx.core;
     requires eu.hansolo.tilesfx;
     requires com.almasb.fxgl.all;
+    requires de.jensd.fx.glyphs.fontawesome;
 
     opens com.kracz0.desktopwitelonbank to javafx.fxml;
     exports com.kracz0.desktopwitelonbank;
+    exports com.kracz0.desktopwitelonbank.Controllers;
+    exports com.kracz0.desktopwitelonbank.Models;
+    exports com.kracz0.desktopwitelonbank.Views;
 }
