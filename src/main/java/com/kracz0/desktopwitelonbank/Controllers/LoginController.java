ae17925f8883669bdef32d7271c5d9451139f9d9
Krystian Raczyński <43791007+KRacz0@users.noreply.github.com>
2025-04-05 14:09:43 +0200

    Create a login form

--- src/main/java/com/kracz0/desktopwitelonbank/Controllers/LoginController.java
+++ src/main/java/com/kracz0/desktopwitelonbank/Controllers/LoginController.java
@@ -1,4 +1,12 @@
 package com.kracz0.desktopwitelonbank.Controllers;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class LoginController {
+    public TextField login_fld;
+    public TextField password_fld;
+    public Button login_btn;
+    public Label error_lbl;
 }
