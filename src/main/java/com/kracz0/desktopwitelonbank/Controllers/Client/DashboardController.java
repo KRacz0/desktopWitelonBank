5077e4e0335eca5eea98d32d31c0da9f8cf75bbf
Krystian Raczyński <43791007+KRacz0@users.noreply.github.com>
2025-05-11 20:40:37 +0200

    Create a dashboard & sidebar

---
+++ src/main/java/com/kracz0/desktopwitelonbank/Controllers/Client/DashboardController.java
@@ -0,0 +1,18 @@
+package com.kracz0.desktopwitelonbank.Controllers.Client;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.text.Text;
+
+public class DashboardController {
+    public Text user_name;
+    public Label card_balance;
+    public Label card_number;
+    public Label card_acc_number;
+    public Button transactions_btn;
+    public ListView transaction_listview;
+    public Label income_lbl;
+    public Label expensen_lbl;
+    public Label login_date;
+}
